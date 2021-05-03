eaeb2b6dca4c06d69eedd94890319fd6afdbc18e
Daniel McPherson <daniel.mcpherson@live.co.uk>
2021-05-03 10:58:00 +0100

    Hopfully Final Commit

--- Kanye West Quoter/MainActivity.cs
+++ Kanye West Quoter/MainActivity.cs
@@ -1,6 +1,6 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Views;
+using System.Net.Http;
 using Android.Runtime;
 using Android.Widget;
 using AndroidX.AppCompat.App;
@@ -17,11 +17,14 @@ namespace Kanye_West_Quoter
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
+            // Inilitilises some variables to pass to the getQuote() function
             TextView quoteTextView = FindViewById<TextView>(Resource.Id.quoteTextView);
+            HttpClient httpClient = new HttpClient();
 
+            // Sets an onClickListener for when the quoteButton is pressed
             Button quoteButton = FindViewById<Button>(Resource.Id.quoteButton);
             quoteButton.Click += (sender, e) => {
-                quoteTextView.Text = "It Works";
+                getQuote(quoteTextView, httpClient);
             };
 
         }
@@ -33,7 +36,25 @@ namespace Kanye_West_Quoter
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
-        private void getQuote() {
+        private void getQuote(TextView quoteTextView, HttpClient httpClient) {
+            // Calls the Kanye.REST api to get the quote
+            var response = httpClient.GetAsync("https://api.kanye.rest/");
+            var result = response.Result.Content.ReadAsStringAsync();
+
+            // Gets the current and new quote
+            string oldQuote = quoteTextView.Text;
+            string newQuote = result.Result.Replace("{", "").Replace("}", "").Replace("quote", "").Replace(":", "").Trim('"');
+
+            // Makes sure the new quote is new
+            while(oldQuote == newQuote) {
+                response = httpClient.GetAsync("https://api.kanye.rest/");
+                result = response.Result.Content.ReadAsStringAsync();
+
+                newQuote = result.Result;
+            }
+
+            // Adds it to the quoteTextView
+            quoteTextView.Text = newQuote;
 
         }
 
